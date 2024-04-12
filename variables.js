8f8fe8b2537e18cbdb5e3bf1154791671ea488f3
Ryan Drake <ryan.drake@netmatters-scs.com>
2024-04-12 10:12:26 +0100

    JS additions

---
+++ variables.js
@@ -0,0 +1,7 @@
+const $burger = $(".hamburger");
+const $hamburgerContainer = $(".nav-container-burger");
+const $bodyAll = $(".main-body");
+const $sideBar = $(".sidebar");
+const $headerAll = $(".sticky-container");
+let lastScrollTop = 0;
+let scrollThreshold = 50;
